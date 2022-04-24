7401ce464eb0e97e018650f943bf780167065ab5
NeroCayn <86295984+NeroVCaynic@users.noreply.github.com>
2022-04-25 02:39:08 +0300

    added extra functions
    emotes, selection delete, word count added

--- TextEditor.py
+++ TextEditor.py
@@ -1,16 +1,19 @@
 import PySimpleGUI as sg
 from pathlib import Path
 import os
+from re import search
 
 menubox = [
 	['File', ['Open','Save']],
-	['Edit', ['Word Count']]
+	['Edit', ['Word Count', 'Delete']]
 ]
+
+txtmenu = ['emotes',['(｡◕‿‿◕｡)','(╥﹏╥)','(̿▀̿ ̿Ĺ̯̿̿▀̿ ̿)̄','༼ つ ◕_◕ ༽つ','୧༼ಠ益ಠ╭∩╮༽','(ㆆ _ ㆆ)']]
 
 setup = [
 	[sg.Menu(menubox, key='-menu-')],
 	[sg.Text('Untitled', key='-txtname-')],
-	[sg.Multiline(no_scrollbar = True, key='-txtbox-', size=(80,40))]
+	[sg.Multiline(no_scrollbar = True, key='-txtbox-', size=(60,30), font=('Arial', 14), autoscroll=True, enable_events = True, right_click_menu=txtmenu)]
 ]
 
 window = sg.Window('Text Editor', setup)
@@ -23,8 +26,12 @@ while True:
 		filePath = sg.popup_get_file('Open', no_window=True)
 		if filePath:
 			file = Path(filePath)
-			window['-txtbox-'].update(file.read_text())
-			window['-txtname-'].update(filePath.split('/')[-1])
+			if os.path.isfile(filePath) == True:
+				if search(filePath.split('/')[-1], '.txt') == True:
+					window['-txtbox-'].update(file.read_text())
+					window['-txtname-'].update(filePath.split('/')[-1])
+				else:
+					sg.Popup("Wrong File")
 
 	if events == 'Save':
 		filePath = sg.popup_get_file('Save as', no_window=True, save_as=True)
@@ -32,6 +39,26 @@ while True:
 		if os.path.isfile(filePath) == True:
 			file.write_text(values['-txtbox-'])
 			window['-txtname-'].update(filePath.split('/')[-1])
+
+	if events == 'Word Count':
+		words = values['-txtbox-'].replace('\n', ' ').split(' ')
+		for count, value in enumerate(''.join(words)):
+			counts = count
+		sg.Popup(f'The total words are {counts+1}')
+
+
+	if events == 'Delete':
+		try:
+			selection = window['-txtbox-'].Widget.selection_get()
+			txt = values['-txtbox-'].replace('\n', ' ').split(' ')
+			ind = txt.index(selection)
+			txt.pop(ind)
+		except:
+			pass
+
+	if events in txtmenu[1]:
+		insert = values['-txtbox-'] + events
+		window['-txtbox-'].update(insert)
 
 	if events == sg.WIN_CLOSED:
 		break
