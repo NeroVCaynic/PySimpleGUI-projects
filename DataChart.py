14b29573373ec01996d1d77fe88624da96d41127
NeroCayn <86295984+NeroVCaynic@users.noreply.github.com>
2022-05-06 15:57:45 +0300

    Code Update
    things added

--- DataChart.py
+++ DataChart.py
@@ -1,3 +1,28 @@
 import PySimpleGUI as sg
 from pathlib import Path as path
 import pandas as pd
+
+Menu = [['File', ['Open','Save as','Exit']],
+	['Themes', ['Dark','Blue','Light']]
+]
+
+layout = [[sg.Menu(Menu, key='-MENU-')]]
+
+window = sg.Window('Data Chart', layout)
+
+while True:
+	events, values = window.read()
+
+	if events == 'Open':
+		pass
+
+	if events == 'Save as':
+		pass
+
+	if events in ['Dark','Blue','Light']:
+		print(events)
+
+	if events in [sg.WIN_CLOSED, 'Exit']:
+		break
+
+window.close()
