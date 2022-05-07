8e9377a25eb2736815405c9eb3f50ba8225e1988
NeroCayn <86295984+NeroVCaynic@users.noreply.github.com>
2022-05-07 13:33:02 +0300

    Update DataChart.py
    Testing and framework

--- DataChart.py
+++ DataChart.py
@@ -1,26 +1,61 @@
 import PySimpleGUI as sg
 from pathlib import Path as path
 import pandas as pd
+import matplotlib as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-Menu = [['File', ['Open','Save as','Exit']],
-	['Themes', ['Dark','Blue','Light']]
-]
+def update_figure(data):
+	axes = fig.axes
+	x = [i[0] for i in data]
+	y = [int(i[1]) for i in data]
+	axes[0].plot(x,y,'r-')
+	figure_canvas_agg.draw()
+	figure_canvas_agg.get_tk_widget().pack()
 
-layout = [[sg.Menu(Menu, key='-MENU-')]]
+def cWindow(theme='LightGrey'):
+	sg.theme(theme)
 
-window = sg.Window('Data Chart', layout)
+	Menu = [['File', ['Open','Save as','Exit']],
+		['Themes', ['Dark','Blue','LightGrey']]
+	]
+
+	Tab1 = sg.Tab('View', [[sg.Text('Text', key='-TEXT-')],
+		[sg.Button('Button', key='-BUTTON-')]
+	])
+	
+	Tab2 = sg.Tab('Data', [[sg.Checkbox('1', key='-1-'), sg.Checkbox('2', key='-2-'), sg.Checkbox('3', key='-3-')],
+		[sg.Frame('Data Visualization', [[sg.Canvas(size=(400,400), key='-CHART-')]])]
+	])
+	
+	layout = [[sg.Menu(Menu, key='-MENU-')],
+		[sg.TabGroup([[Tab1, Tab2]])]
+	]
+
+	return sg.Window('Data Chart', layout, finalize=True)
+
+window = cWindow()
+
+fig = plt.figure.Figure(figsize = (5,4))
+fig.add_subplot(111).plot([],[])
+figure_canvas_agg = FigureCanvasTkAgg(fig,window['-CHART-'].TKCanvas)
+figure_canvas_agg.draw()
+figure_canvas_agg.get_tk_widget().pack()
 
 while True:
 	events, values = window.read()
 
-	if events == 'Open':
+	if events in ['-1-', '-2-', '-3-']:
 		pass
+
+	if events == 'Open':
+		sg.popup_get_file('Open', no_window=True)
 
 	if events == 'Save as':
-		pass
+		sg.popup_get_file('Save As', save_as=True, no_window=True)
 
-	if events in ['Dark','Blue','Light']:
-		print(events)
+	if events in ['Dark','Blue','LightGrey']:
+		window.close()
+		window = cWindow(events)
 
 	if events in [sg.WIN_CLOSED, 'Exit']:
 		break
