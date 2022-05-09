f698a1d5fe45fc08a52c4e67067c039ce43b7099
NeroCayn <86295984+NeroVCaynic@users.noreply.github.com>
2022-05-09 18:26:24 +0300

    Update DataChart.py
    canvas testing

--- DataChart.py
+++ DataChart.py
@@ -5,11 +5,19 @@ import pandas as pd
 import matplotlib as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
-def update_figure(data):
+def update_figure():
 	axes = fig.axes
-	x = [i[0] for i in data]
-	y = [int(i[1]) for i in data]
+	x = [100, 200, 300, 400]
+	y = [10, 250, 300, 400]
 	axes[0].plot(x,y,'r-')
+	figure_canvas_agg.draw()
+	figure_canvas_agg.get_tk_widget().pack()
+
+def create_plot():
+	global fig, figure_canvas_agg
+	fig = plt.figure.Figure(figsize = (5,4))
+	fig.add_subplot(111).plot([],[])
+	figure_canvas_agg = FigureCanvasTkAgg(fig,window['-DATACHART-'].TKCanvas)
 	figure_canvas_agg.draw()
 	figure_canvas_agg.get_tk_widget().pack()
 
@@ -29,7 +37,8 @@ def cWindow(theme='LightGrey1'):
 			key='-TABLECONTENT-', auto_size_columns=True, expand_x = True, expand_y = True, justification = "left",)]], expand_x = True, expand_y = True)]
 	])
 	
-	Tab2 = sg.Tab('Data', [[sg.Frame('Data Form', [[sg.Spin(heading, key='-DATAHEADx-', expand_x = True), sg.Spin(heading, key='-DATAHEADy-', expand_x = True), sg.Spin(['Plot', 'Pie', 'Bar'],
+	Tab2 = sg.Tab('Data', [[sg.Frame('Data Form', [[sg.Spin(heading, key='-DATAHEADx-', expand_x = True), sg.Spin(heading, key='-DATAHEADy-', expand_x = True), 
+		sg.Spin(['Plot', 'Pie', 'Bar'],
 	 expand_x = True)], [sg.Button('Submit', key='-DATASUBMIT-')]], expand_x = True)],
 		[sg.Frame('Data Visualization', [[sg.Canvas(size=(400,400), key='-DATACHART-')]])]
 	])
@@ -42,11 +51,7 @@ def cWindow(theme='LightGrey1'):
 
 window = cWindow()
 
-fig = plt.figure.Figure(figsize = (5,4))
-fig.add_subplot(111).plot([],[])
-figure_canvas_agg = FigureCanvasTkAgg(fig,window['-DATACHART-'].TKCanvas)
-figure_canvas_agg.draw()
-figure_canvas_agg.get_tk_widget().pack()
+create_plot()
 
 while True:
 	events, values = window.read()
@@ -71,6 +76,10 @@ while True:
 	if events in ['DarkGrey9','DarkGreen6','LightGrey1']:
 		window.close()
 		window = cWindow(events)
+		create_plot()
+
+	if events == '-DATASUBMIT-':
+		update_figure()
 
 	if events in [sg.WIN_CLOSED, 'Exit']:
 		break
